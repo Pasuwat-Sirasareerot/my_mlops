49952e97cf03967a2b4fa033132778cbfd1239f6
Yarida <yarida@MacBook-Pro-khxng-Yarida.local>
2026-02-17 14:53:00 +0700

    exception handling, logging

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,14 @@
+import sys
+from networksecurity.logging import logger
+
+class ProjectException(Exception):
+    def __init__(self, error_message: str, error_detail: sys):
+        self.error_message = error_message
+        _, _, exc_tb = error_detail.exc_info()
+
+        self.line_number = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occurred in file: {self.file_name} at line number: {self.line_number} with error message: {self.error_message}".format(
+        self.file_name, self.line_number, str(self.error_message))

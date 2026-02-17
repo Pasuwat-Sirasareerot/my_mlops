139d8c14285302c598de9189e8597dcb55cad504
Yarida <yarida@MacBook-Pro-khxng-Yarida.local>
2026-02-18 00:13:20 +0700

    data insertion

--- push_data.py
+++ push_data.py
@@ -28,5 +28,33 @@ class NetworkDataExtract():
         try:
             ds = pd.read_csv(file_path)
             ds.reset_index(inplace=True, drop=True)
+            records = list(json.loads(ds.T.to_json()).values())
+            return records
         except Exception as e:
             raise ProjectException(e, sys)
+        
+    def insert_data_to_database(self, records, database, collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGODB_URL)
+            self.database =self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return (len(self.records))
+        except Exception as e:
+            raise ProjectException(e, sys)
+        
+if __name__ == "__main__":
+    FILE_PATH = "/Users/yarida/Desktop/mlops_learning/network_data/phisingData.csv"
+    DATABASE = "pasuwat_db"
+    COLLECTION = "phishing_data"
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json(file_path=FILE_PATH)
+    print(f"{len(records)} records found in csv file")
+    no_record = networkobj.insert_data_to_database(records, DATABASE, COLLECTION)
+    print(f"{no_record} records inserted to database {DATABASE} and collection {COLLECTION}")
